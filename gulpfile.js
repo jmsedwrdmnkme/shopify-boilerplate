0a8eeb57113ac2ab505735e9b982a93986717cdb
James Monk <90908782+jmsedwrdmnkme@users.noreply.github.com>
2024-06-28 15:04:57 +0100

    Removal of sprite and rename of logical paths/update

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ import compiler from 'webpack';
 import webpack from 'webpack-stream';
 import concat from 'gulp-concat';
 import uglify from 'gulp-uglify';
-import svgsprite from 'gulp-svg-sprite';
 import imagemin, {gifsicle, mozjpeg, optipng, svgo} from 'gulp-imagemin';
 
 // Clean
@@ -16,7 +15,7 @@ export const clean = () => deleteAsync([ 'assets' ]);
 
 // Styles
 export function styles() {
-  return src('src/css/*', {encoding: false})
+  return src('src/styles/*', {encoding: false})
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(cleanCSS())
     .pipe(concat('styles.css.liquid'))
@@ -25,7 +24,7 @@ export function styles() {
 
 // Scripts
 export function scripts() {
-  return src('src/js/*', { encoding: false, sourcemaps: true })
+  return src('src/scripts/*', { encoding: false, sourcemaps: true })
     .pipe(webpack({}, compiler, function(err, stats) {}))
     .pipe(uglify())
     .pipe(concat('scripts.js.liquid'))
@@ -33,19 +32,8 @@ export function scripts() {
 }
 
 // Assets
-export function sprite() {
-  return src('src/sprite/**/**/*.svg', {encoding: false})
-    .pipe(svgsprite({
-      shape: { spacing: { padding: 5 } },
-      mode: { symbol: true },
-      svg: { xmlDeclaration: false, doctypeDeclaration: false, namespaceIDs: false, namespaceClassnames: false }
-    }))
-    .pipe(concat('sprite.liquid'))
-    .pipe(dest('snippets/'))
-}
-
 export function images() {
-  return src('src/img/**/**/*', {encoding: false})
+  return src('src/images/*', {encoding: false})
     .pipe(imagemin([
       //gifsicle({interlaced: true}),
       mozjpeg({quality: 75, progressive: true}),
@@ -60,12 +48,11 @@ export function images() {
 }
 
 function watchFiles() {
-  watch('src/css/**/*', styles);
-  watch('src/js/**/*', scripts);
-  watch('src/sprite/**/*.svg', sprite);
-  watch('src/img/**/*', images);
+  watch('src/styles/**/*', styles);
+  watch('src/scripts/**/*', scripts);
+  watch('src/images/*', images);
 }
 
-const build = series(clean, parallel(sprite, images, styles, scripts), watchFiles);
+const build = series(clean, parallel(images, styles, scripts), watchFiles);
 
 export default build;
