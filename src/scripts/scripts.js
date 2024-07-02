1c43f6eeb3b6a6752dd0b9f91b7e0122e77c2a3e
James Monk <90908782+jmsedwrdmnkme@users.noreply.github.com>
2024-07-02 14:16:39 +0100

    Javascript update

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -5,15 +5,12 @@
 addToCartAjax = () => {
   let addToCartForm = document.querySelector('form[action$="/cart/add"]');
   let formData = new FormData(addToCartForm);
-  const dialog = addToCartForm.querySelector('dialog');
 
   fetch(window.Shopify.routes.root + 'cart/add.js', {
     method: 'POST',
     body: formData
   })
   .then(response => {
-    dialog.open();
-    setTimeout(dialog.close(), 3000);
     return response.json();
   })
   .catch((error) => {
@@ -26,5 +23,9 @@ const addToCartBtn = document.querySelector('form.shopify-product-form input[typ
 addToCartBtn.addEventListener('click', (event) => {
   event.preventDefault();
   addToCartAjax();
-  console.log('Item(s) added');
+
+  const dialog = document.querySelector('dialog');
+
+  dialog.open();
+  setTimeout(dialog.close(), 3000);
 });
