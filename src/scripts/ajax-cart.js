3ddd70dc354843263005824bba8de3ce136839b4
James Monk <90908782+jmsedwrdmnkme@users.noreply.github.com>
2025-04-01 18:57:56 +0100

    Ajax cart globalisation

---
+++ src/scripts/ajax-cart.js
@@ -0,0 +1,13 @@
+/*
+ * Add to cart
+*/
+document.querySelectorAll('form[action$="/cart/add"]').forEach((form) => {
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    await fetch('cart/add.js', {
+      method: 'POST',
+      body: new FormData(form)
+    });
+  });
+});
