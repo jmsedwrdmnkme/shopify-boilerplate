b6cd92f2dc479c4ad7040c54fb899995e951fd95
James Monk <90908782+jmsedwrdmnkme@users.noreply.github.com>
2025-04-01 18:59:31 +0100

    Ajax update

--- src/scripts/ajax-cart.js
+++ src/scripts/ajax-cart.js
@@ -5,9 +5,15 @@ document.querySelectorAll('form[action$="/cart/add"]').forEach((form) => {
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    await fetch('cart/add.js', {
+    await fetch(window.Shopify.routes.root + 'cart/add.js', {
       method: 'POST',
       body: new FormData(form)
+    })
+    .then(response => {
+      return response.json();
+    })
+    .catch((error) => {
+      console.error('Error:', error);
     });
   });
 });
